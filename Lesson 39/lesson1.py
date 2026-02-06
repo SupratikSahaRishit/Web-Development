23f09ce076b258a4726221832bfd0669396d852a
SupratikSahaRishit <diras2002@gmail.com>
2026-02-06 21:49:22 +0600

    done

---
+++ Lesson 39/lesson1.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcomw to the world of programming!")
+#Print a number 
+print(4)
+#Next line after print
+print("Hello Python \n")
+#Pass multiple arguments for printing 
+print("hello",5)
+#End argument of Print Statement 
+print("welcome to", end="****")
---
+++ Lesson 39/lesson3.py
@@ -0,0 +1,7 @@
+import keyword
+#print all the python keywords
+print("Python keywords are ....\n")
+print(keyword.kwlist)
+python keywords are.... 
+['False','None','True','and','as','assert','async','await','break','glass','continue','def','del','elif','else','except','finally','fox','from','global','if','import','in','is','lambda','nonlocal','not','pr','pass','raise','return','try','while','with','yield']
+>
