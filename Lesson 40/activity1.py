b53ca0997408f71d76b012435d65dfb0b48c6c8e
SupratikSahaRishit <diras2002@gmail.com>
2026-02-11 23:05:25 +0600

    done

---
+++ Lesson 40/activity1.py
@@ -0,0 +1,35 @@
+#Datatypes
+a = 5 
+print("type of a: " , type(a))
+b = 2.5 
+print("type of a: " , type(b))
+c = "coding"
+print("type of a: " , type(c))
+d = True
+print("type of a: " , type(d))
+e = False
+print("type of a: " , type(e))
+print("end ********************")
+#Typecasting of data
+a = 4 
+b=float(a)
+print("the value of b is ", b)
+a = 7.8
+b=int(a)
+print("the value of b is ", b)
+a = "5"
+b = "7"
+c = a+b
+print("the result is ", c)
+print(eval("5+7"))
+#String operation 
+a = "supratik"
+b = "saha"
+c = a+b
+print("the result is", c)
+text = input["Enter a string: "]
+print(text)
+revText = text[::-1]
+text = revText
+print("Reverse of Given String is : ")
+print(text)
