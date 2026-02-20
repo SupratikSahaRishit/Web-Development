a7ff8f716857f5689ea0f8a14f363f3970f11deb
SupratikSahaRishit <diras2002@gmail.com>
2026-02-20 21:41:44 +0600

    done

---
+++ Lesson 43/act2.py
@@ -0,0 +1,19 @@
+a = 10
+b = 12
+c = 0
+if a and b and c :
+ print("All numbers have boolean value like True")
+else :
+ print("Atleast one number have boolean value like False")
+a = 10 
+b = 10 
+c = 0
+if a > 0 or b > 0 :
+  print("Either of the number is greater than zero ")
+else : 
+  print("No number is greater than zero ")
+if b > 0 or c > 0 :
+    print("Either of the number is greater than zero ")
+else : 
+  print("No number is greater than zero ")
+  
