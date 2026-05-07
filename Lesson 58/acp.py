6f5d717ad45759c033517a1f0fa51d3e62dfd4c9
SupratikSahaRishit <diras2002@gmail.com>
2026-05-07 19:42:45 +0600

    done

--- Lesson 58/acp.py
+++ Lesson 58/acp.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4]
-squares = []
-for x in numbers:
-    squares.append(x*2)
-print(squares)
+test_dict = {'Sentosa' : 2, 'Island' : 2, 'is' : 2, 'beautiful' :  1}
+print("The original dictionary : " + str(test_dict))
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+print("Frequency of K is : " + str(res))
