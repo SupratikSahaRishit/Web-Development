dcef85a678b99b0d92e21d8741beb85f199c9964
SupratikSahaRishit <diras2002@gmail.com>
2026-05-29 21:44:09 +0600

    done

---
+++ Lesson 65/acp.py
@@ -0,0 +1,15 @@
+class ReverseString:
+    
+    def reverse_words(self, text):
+        words = text.split()
+    
+        reversed_words = words[::-1]
+        
+        return " ".join(reversed_words)
+
+obj = ReverseString()
+
+text = "Python is very easy"
+
+print("Original String:", text)
+print("Reversed String:", obj.reverse_words(text))
